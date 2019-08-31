102ef922d4a46199513c322a9147c1c95a157e37
richardbonneau <richardbonneau@hotmail.com>
2019-08-31 18:42:49 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="main-logo" src="https://image.shutterstock.com/image-vector/r-logo-initial-letter-design-260nw-780839596.jpg" />
+       <div className="links-wrapper">
+       <a className="header-link" href="#">Work</a>
+        <a className="header-link" href="#">Contact</a>
+       </div>
       </header>
+      <h1 className="main-intro">I'm Richard Bonneau, a full stack <span className="intro-span">Web Developer</span> living in Montreal, currently working as a <span className="intro-span">freelancer</span></h1>
+    <h2>Featured Work</h2>
+    <h2>Additional Work</h2>
+    <h2>Looking to start a project? Let's talk</h2>
     </div>
   );
 }
