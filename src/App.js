1cce2efced7a47fb0fb68788480e9a7c8b34e641
richardbonneau <richardbonneau@hotmail.com>
2019-09-04 18:06:21 -0400

    changed layout, added hero

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="hero">
       <header className="App-header">
-        <img className="main-logo" src="https://image.shutterstock.com/image-vector/r-logo-initial-letter-design-260nw-780839596.jpg" />
+        
+       <div>Richard Bonneau</div>
        <div className="links-wrapper">
        <a className="header-link" href="#">Work</a>
         <a className="header-link" href="#">Contact</a>
        </div>
+       
       </header>
-      <h1 className="main-intro">I'm Richard Bonneau, a full stack <span className="intro-span">Web Developer</span> living in Montreal, currently working as a <span className="intro-span">freelancer</span></h1>
-    <h2>Featured Work</h2>
-    <h2>Additional Work</h2>
+      <h1 className="main-intro">I am a full stack <span className="intro-span">Web Developer</span> living in Montreal, currently working as a <span className="intro-span">freelancer</span></h1>
+      <button>Hire me</button>
+      <div className="hero-icons">
+      <div>LinkedIn </div>
+      <div>GitHub </div>
+      <div>repl?</div>
+      </div>
+      
+      </div>
+    <h2>Latest Work</h2>
     <h2>Looking to start a project? Let's talk</h2>
+    <div>Connect on LinkedIn</div>
+    <div>Message on Facebook</div>
+    <div>Send an Email</div>
     </div>
   );
 }
 
 export default App;
+
